760fe2f48cfd5f0f5ac5185d794aa229b3aaac63
abdel-b <ab7975@nyu.edu>
2022-02-17 17:36:51 -0500

    initializing frontend

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import Rooms from './pages/Rooms/Rooms';
+import Users from './pages/Users/Users';
+
+import {backendurl} from './config';
+
+import './App.css';
+
+function App() {
+  console.log(backendurl);
+  return (
+    <div className="root">
+      <div className="content">
+        <Router>
+          <Switch>
+            <Route exact={true} path={'/'}>
+              <Home />
+            </Route>
+            <Route exact={true} path={'/rooms'}>
+              <Rooms />
+            </Route>
+            <Route exact={true} path={'/users'}>
+              <Users />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const backendurl = process.env.REACT_APP_BACKEND_URL || 'https://demo-repo23.herokuapp.com/';
