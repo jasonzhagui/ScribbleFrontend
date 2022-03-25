aba61aba0535b4663769c481daabb9803bb58e6d
abdel-b <ab7975@nyu.edu>
2022-03-24 22:09:14 -0400

    adding post request button

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -4,6 +4,7 @@ import {useHistory} from 'react-router-dom';
 
 
 import PageTitle from '../../components/PageTitle/PageTitle';
+import {backendurl} from '../../config';
 
 import './gallery.css';
 
@@ -11,6 +12,28 @@ export default function Gallery() {
 
   const history = useHistory();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [refresh, setRefresh] = useState(undefined);
+
+  const [category, setCategory] = useState('');
+  const [newName, setNewName] = useState('');
+  const [newLink, setNewLink] = useState('');
+
+  useEffect(() => {}, [refresh])
+
+  const handleCreateLayer = () => {
+    axios.post(`${backendurl}/layers/create/${category}/${newName}/${newLink}`)
+      .then(() => {
+        console.log('category');
+        console.log(category);
+        console.log('newName');
+        console.log(newName);
+        console.log('newLink');
+        console.log(newLink);
+        setIsModalOpen(false);
+        setRefresh(refresh + 1);
+      })
+  }
 
   return (
     <div className="content">
@@ -25,6 +48,31 @@ export default function Gallery() {
         >
           {"<--"}Go Back Home
         </button>
+      </div>
+
+      {isModalOpen && 
+        <div className="create-modal">
+          <input
+            value1={category}
+            onChange={(e) => { setCategory(e.target.value1);}}
+          />
+          <input
+            value2={newName}
+            onChange={(e) => { setNewName(e.target.value2); }}
+          />
+          <input
+            value3={newLink}
+            onChange={(e) => { setNewLink(e.target.value3) }}
+          />
+          <div className="create-actions">
+            <button className="button" onClick={handleCreateLayer}>Create New Layer</button>
+            <button className="button" onClick={() => setIsModalOpen(false)}> Cancel </button>
+          </div>
+        </div>
+      }
+
+      <div>
+        <button className="page-button" onClick={() => setIsModalOpen(true)}> input </button>
       </div>
 
     </div>
