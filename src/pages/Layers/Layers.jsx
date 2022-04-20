c9eaed75659fe4f31bfd1b3ff8659fbf18da167e
Jason Zhagui <jasonzhagui@gmail.com>
2022-04-20 00:50:27 -0400

    changing Draw Scribble page layout

--- src/pages/Layers/Layers.jsx
+++ src/pages/Layers/Layers.jsx
@@ -1,49 +1,49 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Select from 'react-select';
 
 import PageTitle from '../../components/PageTitle/PageTitle';
-import {backendurl} from '../../config';
+import { backendurl } from '../../config';
 
 import './layers.css';
 import LayerItem from '../../components/LayerItem/LayerItem';
 
-import {useSession} from '../../context/sessioncontext';
+import { useSession } from '../../context/sessioncontext';
 
 export default function Layers() {
 
   const [error, setError] = useState(undefined);
-  
+
   const [body, setBody] = useState(undefined);
   const [head, setHead] = useState(undefined);
   const [eyes, setEyes] = useState(undefined);
-  const [mouth, setMouth] = useState(undefined); 
+  const [mouth, setMouth] = useState(undefined);
 
   const [dropdown, setDropdown] = useState([]);
 
   const [refresh, setRefresh] = useState(undefined);
 
   //const [scribble, setScribble] = useState([]);
-  const scribble = [body,head,eyes,mouth];
+  const scribble = [body, head, eyes, mouth];
 
   const history = useHistory();
 
-  const refreshPage = ()=>{
+  const refreshPage = () => {
     window.location.reload();
   }
-  
+
   function navigateToPage(path) {
     history.push(path);
   }
 
   const session = useSession();
-  
+
   useEffect(() => {
     axios.get(`${backendurl}/layers/dropdownList`)
       .then((response) => {
-        if (response.data){
+        if (response.data) {
 
           setDropdown(response.data)
         }
@@ -52,7 +52,7 @@ export default function Layers() {
     axios.get(`${backendurl}/layers/body`)
       .then((response) => {
         console.log(response.data);
-        if (response.data){
+        if (response.data) {
           let bodyValues = Object.values(response.data[0])
           setBody(bodyValues[Math.floor(Math.random() * bodyValues.length)]);
         }
@@ -61,7 +61,7 @@ export default function Layers() {
     axios.get(`${backendurl}/layers/head`)
       .then((response) => {
         console.log(response.data);
-        if (response.data){
+        if (response.data) {
           let headValues = Object.values(response.data[0])
           setHead(headValues[Math.floor(Math.random() * headValues.length)]);
         }
@@ -70,7 +70,7 @@ export default function Layers() {
     axios.get(`${backendurl}/layers/eyes`)
       .then((response) => {
         console.log(response.data);
-        if (response.data){
+        if (response.data) {
           let eyesValues = Object.values(response.data[0])
           setEyes(eyesValues[Math.floor(Math.random() * eyesValues.length)]);
         }
@@ -79,15 +79,15 @@ export default function Layers() {
     axios.get(`${backendurl}/layers/mouth`)
       .then((response) => {
         console.log(response.data);
-        if (response.data){
+        if (response.data) {
           let mouthValues = Object.values(response.data[0])
           setMouth(mouthValues[Math.floor(Math.random() * mouthValues.length)]);
         }
       })
-    }, 
-  [refresh])
+  },
+    [refresh])
 
-  function testSetScribble(){
+  function testSetScribble() {
     console.log("scribble");
     console.log(scribble);
 
@@ -98,7 +98,7 @@ export default function Layers() {
 
       <div className="layers-header">
         <PageTitle
-          text="Layers"
+          text="Draw a Scribble"
         />
         <button
           onClick={() => history.push('/')}
@@ -113,61 +113,66 @@ export default function Layers() {
           <p>{error.toString()}</p>
         </div>
       )}
-      
-      <div className='image-center'>
-        <LayerItem
-          body = {body}
-          head = {head}
-          eyes = {eyes}
-          mouth = {mouth}
-        />
+
+      <div class="flex-container">
+
+        <div class="flex-item-right">
+          <div className='image-center'>
+            <LayerItem
+              body={body}
+              head={head}
+              eyes={eyes}
+              mouth={mouth}
+            />
+          </div>
+        </div>
+        <div class="flex-item-left">
+          <div className="page-dropdown">
+            <h2>Body:</h2>
+            <Select
+              isSearchable={false}
+              placeholder='Choose a body'
+              options={dropdown[0]}
+              onChange={(e) => { setBody(e.value) }}
+            />
+          </div>
+
+          <div className="page-dropdown">
+            <h2>Head:</h2>
+            <Select
+              isSearchable={false}
+              placeholder='Choose a head'
+              options={dropdown[1]}
+              onChange={(e) => { setHead(e.value) }}
+            />
+          </div>
+
+          <div className="page-dropdown">
+            <h2>Eyes:</h2>
+            <Select
+              isSearchable={false}
+              placeholder='Choose a eyes'
+              options={dropdown[2]}
+              onChange={(e) => { setEyes(e.value) }}
+            />
+          </div>
+
+          <div className="page-dropdown">
+            <h2>Mouth:</h2>
+            <Select
+              isSearchable={false}
+              placeholder='Choose a mouth'
+              options={dropdown[3]}
+              onChange={(e) => { setMouth(e.value) }}
+            />
+          </div>
+        </div>
       </div>
 
-      <div>
-      <button className="page-button" onClick={refreshPage}> Draw Random Scribble </button>
-      </div>
+      <div id="container">
+        <button id = "options" onClick={refreshPage}> Draw Random Scribble </button>
 
-      <div className="page-dropdown">
-        <h2>Body:</h2>
-        <Select
-          isSearchable = {false}
-          placeholder = 'Choose a body'
-          options = {dropdown[0]}
-          onChange={(e) => {setBody(e.value)}}
-        />
-      </div>
-
-      <div className="page-dropdown">
-        <h2>Head:</h2>
-        <Select
-          isSearchable = {false}
-          placeholder = 'Choose a head'
-          options = {dropdown[1]}
-          onChange={(e) => {setHead(e.value)}}
-        />
-      </div>
-
-      <div className="page-dropdown">
-      <h2>Eyes:</h2>
-        <Select
-          isSearchable = {false}
-          placeholder = 'Choose a eyes'
-          options = {dropdown[2]}
-          onChange={(e) => {setEyes(e.value)}}
-        />
-      </div>
-
-      <div className="page-dropdown">
-      <h2>Mouth:</h2>
-        <Select
-          isSearchable = {false}
-          placeholder = 'Choose a mouth'
-          options = {dropdown[3]}
-          onChange={(e) => {setMouth(e.value)}}
-        />
-      </div>
-      <div>
-        <button className="page-button" onClick={() => {
+        <button id = "options" onClick={() => {
           testSetScribble();
           navigateToPage('/gallery');
           localStorage.setItem('body', body);
