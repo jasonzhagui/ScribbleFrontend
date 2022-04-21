ba6a50684b84f86e3d78ec16ddf5a50c7d01d3c8
Jason Zhagui <jasonzhagui@gmail.com>
2022-04-21 12:45:01 -0400

    adding random scribbles to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,12 +15,57 @@ export default function Home() {
   const [logged, setLogged] = useState(false);
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [bodies, setBodies] = useState([]);
+  const [heads, setHeads] = useState([]);
+  const [eyes, setEyes] = useState([]);
+  const [mouths, setMouths] = useState([]);
+
 
   useEffect(() => {
     if (localStorage.getItem('username')) {
       setLogged(true);
     }
-  })
+
+    axios.get(`${backendurl}/layers/list`)
+      .then((response) => {
+        if (response.data) {
+
+          let bodyValues = [];
+          let bodyKeys = Object.values(response.data[0])
+          for (let i = 0; i < 9; i++) {
+            bodyValues.push(bodyKeys[Math.floor(Math.random() * bodyKeys.length)]);
+
+          }
+          setBodies(bodyValues);
+
+
+          let headValues = [];
+          let headKeys = Object.values(response.data[1])
+          for (let i = 0; i < 9; i++) {
+            headValues.push(headKeys[Math.floor(Math.random() * headKeys.length)]);
+
+          }
+          setHeads(headValues);
+
+          let eyesValues = [];
+          let eyesKeys = Object.values(response.data[2])
+          for (let i = 0; i < 9; i++) {
+            eyesValues.push(eyesKeys[Math.floor(Math.random() * eyesKeys.length)]);
+
+          }
+          setEyes(eyesValues);
+
+          let mouthValues = [];
+          let mouthKeys = Object.values(response.data[3])
+          for (let i = 0; i < 9; i++) {
+            mouthValues.push(mouthKeys[Math.floor(Math.random() * mouthKeys.length)]);
+
+          }
+          setMouths(mouthValues);
+
+        }
+      })
+  }, [])
 
   const handleLogin = () => {
     axios.get(`${backendurl}/user/${username}/${password}`)
@@ -47,6 +92,9 @@ export default function Home() {
     setLogged(!logged);
   }
 
+  function redirect(){
+    
+  }
   return (
     <div className="content">
       <h1>Scribble</h1>
@@ -148,12 +196,12 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[0]}
+              head={heads[0]}
+              eyes={eyes[0]}
+              mouth={mouths[0]}
               size={300}
             />
           </div>
@@ -162,12 +210,12 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[1]}
+              head={heads[1]}
+              eyes={eyes[1]}
+              mouth={mouths[1]}
               size={300}
             />
           </div>
@@ -176,58 +224,12 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
-              size={300}
-            />
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="flex-home-container">
-
-        <div class="flex-home-item-right">
-
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
-            <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
-              size={300}
-            />
-          </div>
-
-        </div>
-
-        <div class="flex-home-item-right">
-
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
-            <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
-              size={300}
-            />
-          </div>
-
-        </div>
-
-        <div class="flex-home-item-right">
-
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
-            <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[2]}
+              head={heads[2]}
+              eyes={eyes[2]}
+              mouth={mouths[2]}
               size={300}
             />
           </div>
@@ -240,12 +242,12 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[3]}
+              head={heads[3]}
+              eyes={eyes[3]}
+              mouth={mouths[3]}
               size={300}
             />
           </div>
@@ -254,12 +256,12 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[4]}
+              head={heads[4]}
+              eyes={eyes[4]}
+              mouth={mouths[4]}
               size={300}
             />
           </div>
@@ -268,12 +270,58 @@ export default function Home() {
 
         <div class="flex-home-item-right">
 
-          <div className='image-home-center ' onClick={e => console.log("Clicked")}>
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
             <LayerItem
-              body={"https://i.ibb.co/bFPRST7/Blazer.png"}
-              head={"https://i.ibb.co/Sf7W06W/Blueberry.png"}
-              eyes={"https://i.ibb.co/qLHdSgh/Cool-Shades.png"}
-              mouth={"https://i.ibb.co/sHM8bjW/Angry.png"}
+              body={bodies[5]}
+              head={heads[5]}
+              eyes={eyes[5]}
+              mouth={mouths[5]}
+              size={300}
+            />
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="flex-home-container">
+
+        <div class="flex-home-item-right">
+
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
+            <LayerItem
+              body={bodies[6]}
+              head={heads[6]}
+              eyes={eyes[6]}
+              mouth={mouths[6]}
+              size={300}
+            />
+          </div>
+
+        </div>
+
+        <div class="flex-home-item-right">
+
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
+            <LayerItem
+              body={bodies[7]}
+              head={heads[7]}
+              eyes={eyes[7]}
+              mouth={mouths[7]}
+              size={300}
+            />
+          </div>
+
+        </div>
+
+        <div class="flex-home-item-right">
+
+          <div className='image-home-center' onClick={e => console.log("Clicked")}>
+            <LayerItem
+              body={bodies[8]}
+              head={heads[8]}
+              eyes={eyes[8]}
+              mouth={mouths[8]}
               size={300}
             />
           </div>
