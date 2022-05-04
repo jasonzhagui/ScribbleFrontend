9b8dab3ffd27241d52e410b77e4ff36411fc3ece
Jason Zhagui <jasonzhagui@gmail.com>
2022-05-04 19:47:27 -0400

    changing home page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -171,7 +171,7 @@ export default function Home() {
 
         <div class="flex-home-item-left">
 
-          <h1>Randomly Generated Scribbles</h1>
+          <h2>Randomly Generated Scribbles</h2>
           <br></br>
           <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p1>
@@ -196,7 +196,7 @@ export default function Home() {
         {scribbles.map((scribble) => {
 
             return <div class="flex-home-item-right">
-                      <div className='image-home-center ' onClick={e => window.location.href=`layers/?body=${scribble.body}&head=${scribble.head}&eyes=${scribble.eyes}&mouth=${scribble.mouth}`}>
+                      <div className='image-home-center' onClick={e => window.location.href=`layers/?body=${scribble.body}&head=${scribble.head}&eyes=${scribble.eyes}&mouth=${scribble.mouth}`}>
                         <LayerItem
                           body={scribble.body}
                           head={scribble.head}
