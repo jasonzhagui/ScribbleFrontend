7d6ed89c7783b2959910c94a832f299d0452131a
abdel-b <ab7975@nyu.edu>
2022-05-07 15:34:48 -0400

    replaced filler text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -173,7 +173,7 @@ export default function Home() {
 
           <h2>Randomly Generated Scribbles</h2>
           <br></br>
-          <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <p1> Create cool Scribbles inspired from well-known NFT projects, by combining different attributes. Sign up to save them to your gallery.
           </p1>
           <br></br>
           <button onClick={() => navigateToPage('/layers')} className="page-home-button"> Draw a Scribble </button>
